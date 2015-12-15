fee2ae55337bfd6ac96cb65d9e32c9a81d7b2f00
Guang Yang (AZURE) <guayan@microsoft.com>
2015-12-15 11:36:49 -0800

    add swagger validation

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var h2s = require('../index.js');
 
 var harContent = fs.readFileSync(__dirname + '/sample.har', 'utf8');
 
-var swagger = h2s.generate(harContent);
+var result = h2s.generate(harContent);
 
-console.log(swagger);
+fs.writeFileSync(__dirname + '/swagger.json', JSON.stringify(result.swagger, null, 2));
+fs.writeFileSync(__dirname + '/validation-result.json', JSON.stringify(result.validationResult, null, 2));
