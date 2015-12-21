64dde7e8964bff1aa2fa112e1ecdc9e8cfd2f88f
Guang Yang (AZURE) <guayan@microsoft.com>
2015-12-21 14:31:43 -0800

    throw error instead of swallow it

--- test/test.js
+++ test/test.js
@@ -11,7 +11,11 @@ _.each(harFiles, function (file, index, files) {
 	var sample = fs.readFileSync(__dirname + '/' + file, 'utf8');
   var fileNameWithoutExtension = S(file).strip('.har');
   h2s.generateAsync(sample, function (err, result) {
+    if (err) {
+      throw(err);
+    } else {
     fs.writeFileSync(__dirname + '/' + fileNameWithoutExtension + '-swagger.json', JSON.stringify(result.swagger, null, 2));
     fs.writeFileSync(__dirname + '/' + fileNameWithoutExtension + '-validation-result.json', JSON.stringify(result.validationErrors, null, 2));
+    }
   });
 });
